439e667732dbc0b2d9fa3ad5912b283bef437a64
Huniity <adrien_dejonc@eticlagarve.com>
2025-06-03 18:10:16 +0000

    FIX: Main Fix Adminer Access and Pyproject

--- bartrify/settings.py
+++ bartrify/settings.py
@@ -78,25 +78,25 @@ WSGI_APPLICATION = "bartrify.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 # DATABASES = {
 #     "default": {
-#         #  "ENGINE": "django.db.backends.sqlite3", #<-| For Production
-#         #  "NAME": BASE_DIR / "db.sqlite3",        #<-| use only. Local DB
-
-#         "ENGINE": 'django.db.backends.postgresql',
-#         "NAME": os.getenv('POSTGRES_DB'),
-#         "USER": os.getenv('POSTGRES_USERNAME'),
-#         "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
-#         "HOST": os.getenv('POSTGRES_HOST'),
-#         "PORT": os.getenv('POSTGRES_PORT'),
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+DATABASES = {
+    "default": {
+        #  "ENGINE": "django.db.backends.sqlite3", #<-| For Production
+        #  "NAME": BASE_DIR / "db.sqlite3",        #<-| use only. Local DB
+
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": os.getenv('POSTGRES_DB'),
+        "USER": os.getenv('POSTGRES_USERNAME'),
+        "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
+        "HOST": os.getenv('POSTGRES_HOST'),
+        "PORT": os.getenv('POSTGRES_PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
