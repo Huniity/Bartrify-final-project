84d49573043bb0075931ca6750448e8de9a7ab69
Huniity <adrien_dejonc@eticlagarve.com>
2025-06-04 18:38:47 +0000

    REF: Implement index view and update URL routing; enhance feed template structure

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from core import views
+from django.conf import settings
+
+"""
+Defining all views of the main project.
+"""
+
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+
+"""
+Index view for index.html.
+"""
+
+class IndexView(TemplateView):
+    http_method_names = ["get"]
+    template_name = "core/index.html"
+    logger.info(HttpResponse.status_code)
