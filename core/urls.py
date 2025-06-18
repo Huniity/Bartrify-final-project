c1347621caffc52e6d55c32c9215eac0539536c8
tomás-miguel <tomas_miguel@eticalgarve.com>
2025-06-18 21:07:51 +0000

    CHORE: Feed and Index template

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import RegisterView ,  IndexView, DashboardView
+from core.views import RegisterView ,  IndexView, DashboardView, FeedViews
 from django.conf import settings
 
 """
@@ -9,5 +9,6 @@ Defining all views of the main project.
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
-    path("/Register", RegisterView.as_view(), name="Register"),
+    path("register/", RegisterView.as_view(), name="Register"),
+    path("feed/", FeedViews.as_view(), name="Feed"),
 ]
